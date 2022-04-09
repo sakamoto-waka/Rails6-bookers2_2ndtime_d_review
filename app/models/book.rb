2d020a74fadd762143e55e108da437be441b7498
sakamoto-waka <wakanmhl@gmail.com>
2022-04-09 14:40:22 +0000

    [update] Book.rb validates to rate

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,8 @@ class Book < ApplicationRecord
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  validates :rate, numericality: { less_than_or_equal_to: 5, 
+                                   greater_than_or_equal_to: 1 }
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
