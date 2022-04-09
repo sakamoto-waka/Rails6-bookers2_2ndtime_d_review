2c8e009ec7d3166dbbebf792851d491d95486c3a
sakamoto-waka <wakanmhl@gmail.com>
2022-04-09 14:28:53 +0000

    [fix] visible rate

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+window.$ = window.jQuery = require('jquery'); 
+require('packs/raty') 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -12,7 +14,6 @@ import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application"
-require('packs/raty')
 
 Rails.start()
 Turbolinks.start()
