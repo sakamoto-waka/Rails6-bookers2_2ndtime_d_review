d32f7ff219d16c0dcdad491804d961d2f0f54531
sakamoto-waka <wakanmhl@gmail.com>
2022-04-09 14:01:40 +0000

    [update]application.js require raty

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application"
+require('packs/raty')
 
 Rails.start()
 Turbolinks.start()
