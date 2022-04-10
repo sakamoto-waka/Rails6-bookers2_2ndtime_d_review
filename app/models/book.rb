55546b9bc4633dddccde13e8e609eea7e3203091
sakamoto-waka <wakanmhl@gmail.com>
2022-04-10 03:31:51 +0000

    [update] Book.rb for association

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,9 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :book_tags, dependent: :destroy
+  # これでBook.tagsが取得できる？
+  has_many :tag, through: :book_tags
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
